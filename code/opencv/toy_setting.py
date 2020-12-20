27f1c70cbbe34f799fdae9bcd265480d3189a131
andreaalf <andrea.alfieri97@gmail.com>
2020-12-20 15:04:15 +0100

    Moved to B&W and added random rotation and traslation

--- code/opencv/toy_setting.py
+++ code/opencv/toy_setting.py
@@ -3,102 +3,75 @@ import cv2
 import random
 import matplotlib.pyplot as plt
 import os
+from gate import Gate
+
+
 
 
 def get_ts_image(height, width, max_gates=3, padding=5) -> (np.ndarray, list):
     assert padding >= 0 and isinstance(padding, int)
 
-    img = get_ts_background(height, width, bgr=True, real_background_prob=1.0)
+    img = get_ts_background(height, width, bgr=True, real_background_prob=0.0, black_white=True)
 
-    x_range = (-padding, width+padding)
-    y_range = (-padding, height+padding)
-    if random.random() < 0.05:
-        num_gates = 0
+    if img[0][0][0] == 0:
+        gate = Gate(height, width, perc_of_image_width=0.80, color='white')
     else:
-        num_gates = random.randint(1, max_gates)
+        gate = Gate(height, width, perc_of_image_width=0.80, color='black')
 
-    gate_labels = []
+    # img = print_gate(img, gate, mark_top_corners=True)
+    gate.rand_rotate(horizontal=False)
 
-    for _ in range(num_gates):
-        color = (
-            random.random() * 255,
-            random.random() * 255,
-            random.random() * 255
-        )
+    # img = print_gate(img, gate)
 
-        stroke = random.randint(4, 50)
+    gate.rand_shift(perc=0.20)
 
-        top_left = (
-            int(random.random() * (x_range[1]-x_range[0]) + x_range[0]),
-            int(random.random() * (y_range[1] - y_range[0]) + y_range[0])
-        )
+    img = print_gate(img, gate, mark_top_corners=True)
 
-        if top_left[0] > width/2:   # If the top left point is in the right hand side of the image, we go left
-            top_right = (
-                top_left[0] - abs(int(random.random() * (width) - (width/2))),
-                top_left[1] + int(random.random() * (height) - (height/2)),
-            )
-        else:
-            top_right = (
-                top_left[0] + abs(int(random.random() * (width) - (width / 2))),
-                top_left[1] + int(random.random() * (height) - (height / 2)),
-            )
+    return img, gate.get_labels()
 
-        if top_left[1] > height/2:  # If the top left is in the bottom of the image, we go up
-            bottom_left = (
-                top_left[0],
-                top_left[1] - random.randint(int(-height/2), 0)
-            )
-        else:
-            bottom_left = (
-                top_left[0],
-                top_left[1] + random.randint(0, int(height/2))
-            )
 
-        bottom_right = (
-            top_right[0],
-            bottom_left[1] + (top_right[1]-top_left[1])
-        )
+def print_gate(img:np.ndarray, gate: Gate, mark_top_corners=False) -> np.ndarray:
 
-        img = cv2.line(
-            img,
-            bottom_left,
-            top_left,
-            color,
-            stroke,
-            lineType=cv2.LINE_AA
-        )
+    img = cv2.line(
+        img,
+        gate.bottom_left,
+        gate.top_left,
+        gate.color,
+        gate.stroke,
+        lineType=cv2.LINE_AA
+    )
 
-        img = cv2.line(
-            img,
-            top_left,
-            top_right,
-            color,
-            stroke,
-            lineType=cv2.LINE_AA
-        )
+    img = cv2.line(
+        img,
+        gate.top_left,
+        gate.top_right,
+        gate.color,
+        gate.stroke,
+        lineType=cv2.LINE_AA
+    )
 
-        img = cv2.line(
-            img,
-            top_right,
-            bottom_right,
-            color,
-            stroke,
-            lineType=cv2.LINE_AA
-        )
+    img = cv2.line(
+        img,
+        gate.top_right,
+        gate.bottom_right,
+        gate.color,
+        gate.stroke,
+        lineType=cv2.LINE_AA
+    )
 
-        img = cv2.line(
-            img,
-            bottom_right,
-            bottom_left,
-            color,
-            stroke,
-            lineType=cv2.LINE_AA
-        )
+    img = cv2.line(
+        img,
+        gate.bottom_right,
+        gate.bottom_left,
+        gate.color,
+        gate.stroke,
+        lineType=cv2.LINE_AA
+    )
 
+    if mark_top_corners:
         img = cv2.circle(
             img,
-            top_left,
+            gate.top_left,
             2,
             (255, 0, 0),
             -1
@@ -106,20 +79,16 @@ def get_ts_image(height, width, max_gates=3, padding=5) -> (np.ndarray, list):
 
         img = cv2.circle(
             img,
-            top_right,
+            gate.top_right,
             2,
             (0, 255, 0),
             -1
         )
 
-        gate_labels.append(
-            (top_left, top_right, bottom_right, bottom_left)
-        )
-
-    return img, gate_labels
+    return img
 
 
-def get_ts_background(height, width, real_background_prob=0.0, bgr=False) -> np.ndarray:
+def get_ts_background(height, width, real_background_prob=0.0, bgr=False, black_white=False) -> np.ndarray:
 
     coin = random.random()
     if coin <= real_background_prob:
@@ -127,6 +96,19 @@ def get_ts_background(height, width, real_background_prob=0.0, bgr=False) -> np.
         background = "backgrounds/" + random.choice(backgrounds)
         image = cv2.imread(background)
         image = cv2.resize(image, dsize=(width, height), interpolation=cv2.INTER_CUBIC)
+        return image
+
+    if black_white:
+        image = np.zeros(
+            (height, width, 3),
+            dtype=np.uint8
+        )
+
+        if random.random() > 0.5:
+            image[:, :] = [0, 0, 0]
+        else:
+            image[:, :] = [255, 255, 255]
+
         return image
 
     red = int(random.random() * 255)
@@ -151,9 +133,9 @@ def get_ts_background(height, width, real_background_prob=0.0, bgr=False) -> np.
 
 
 if __name__ == '__main__':
-    image, labels = get_ts_image(540, 860)
+    image, labels = get_ts_image(224, 224)
 
-    # cv2.imwrite("samples/test.png", i)
+    cv2.imwrite("test.png", image)
 
     cv2.imshow("THERE ARE {} GATES".format(len(labels)), image)
     cv2.waitKey()
