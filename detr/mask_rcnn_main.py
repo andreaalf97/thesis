fef11eb1694ce69ecec69d09e35136d0cea2bc18
andreaalf <andrea.alfieri97@gmail.com>
2021-03-19 16:03:25 +0100

    eval maskrcnn 175 epochs

--- detr/mask_rcnn_main.py
+++ detr/mask_rcnn_main.py
@@ -102,14 +102,14 @@ if __name__ == '__main__':
     #############################################
     # EVALUATION PARAMETERS
     # Comment this out for no evaluation
-    # eval_model = "/home/nfs/andreaalfieria/thesis/detr/tmp/maskrcnn_STD_200epochs_checkpoint.pth"
-    eval_model = ""
+    eval_model = "/home/nfs/andreaalfieria/thesis/detr/tmp/maskrcnn_STD_300epochs_checkpoint174.pth"
+    # eval_model = ""
     # eval_model = "/home/andreaalf/Documents/thesis/detr/results/baseline_comparison/maskrcnn_uniform8000_100epochs.pth"
     eval_pkl_path = "/home/nfs/andreaalfieria/STD_TEST_basement.pkl"
     # eval_pkl_path = "/home/andreaalf/Documents/thesis/datasets/normalized_test_2000imgs.pkl"
-    save_results_to = "/home/nfs/andreaalfieria/thesis/detr/tmp/EVAL_maskrcnn_STD_141epochs.pkl"
+    save_results_to = "/home/nfs/andreaalfieria/thesis/detr/tmp/EVAL_maskrcnn_STD_175epochs.pkl"
 
-    seed = 7635  # 43
+    seed = 43
     seed = seed * 2 if eval_model != "" else seed
     torch.manual_seed(seed)
     np.random.seed(seed)
