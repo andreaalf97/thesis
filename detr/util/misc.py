ff22c67a9d90557fd8aa388d7db5b46618c82fa0
andreaalf <andrea.alfieri97@gmail.com>
2021-05-31 13:50:48 +0200

    Working on padding

--- detr/util/misc.py
+++ detr/util/misc.py
@@ -268,7 +268,7 @@ def get_sha():
 
 
 def collate_fn(batch):
-    print(batch*)
+    print(*batch)
     exit(0)
     batch = list(zip(*batch))
     batch[0] = nested_tensor_from_tensor_list(batch[0])
