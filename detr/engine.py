ba1027438f1bce49940ff99d2134e89f65ed47f3
andreaalf <andrea.alfieri97@gmail.com>
2021-06-16 12:18:22 +0200

    Update eval for points

--- detr/engine.py
+++ detr/engine.py
@@ -562,9 +562,10 @@ def match_predictions_optim(pred_logits: torch.Tensor, pred_boxes: torch.Tensor,
     cost_matrix = [[0 for _ in range(len(predictions))] for _ in range(len(gt_boxes))]
     for i, gt_box in enumerate(gt_boxes):  # For each GT mask we find the best match
         for j, (pred_box, _, _) in enumerate(predictions):  # For each prediction mask we compare it
-            iou_score = iou(
+            iou_score = torch.cdist(
                 pred_box,
-                gt_box
+                gt_box,
+                p=1
             )
             cost_matrix[i][j] = iou_score
 
